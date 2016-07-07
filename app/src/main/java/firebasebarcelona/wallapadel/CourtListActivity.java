5ecfb362361697781f9f41874d47440e7bc2dc13
Enrique Álvarez Monedero <Quique@ip-192-168-10-128.eu-west-1.compute.internal>
2016-07-07 17:24:31 +0200

    First commit

---
+++ app/src/main/java/firebasebarcelona/wallapadel/CourtListActivity.java
@@ -0,0 +1,13 @@
+package firebasebarcelona.wallapadel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CourtListActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_court_list);
+  }
+}
